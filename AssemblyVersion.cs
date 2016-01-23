e67351f95af5154973149a0fa51a7d651f551fe9
Matt Davey <matt.davey@fsfe.org>
2016-01-23 20:57:29 +0000

    Adds rake build script

---
+++ AssemblyVersion.cs
@@ -0,0 +1,7 @@
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+[assembly: AssemblyVersion("0.1.0")]
+[assembly: AssemblyFileVersion("0.1.0.8d7b15")]
+[assembly: AssemblyInformationalVersion("0.1.0")]
--- EasyNetQ.Migrations.Runner/Properties/AssemblyInfo.cs
+++ EasyNetQ.Migrations.Runner/Properties/AssemblyInfo.cs
@@ -21,16 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("193d0cb2-f16d-4733-88e8-4f61a4f842f6")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
