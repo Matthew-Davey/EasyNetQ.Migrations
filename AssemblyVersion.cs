1ef214911d14db76f49c6f6fdc815164b5b255e1
Matt Davey <matt.davey@fsfe.org>
2016-01-23 21:39:11 +0000

    Builds version 0.1.0

--- AssemblyVersion.cs
+++ AssemblyVersion.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0.8d7b15")]
+[assembly: AssemblyFileVersion("0.1.0.990906")]
 [assembly: AssemblyInformationalVersion("0.1.0")]
